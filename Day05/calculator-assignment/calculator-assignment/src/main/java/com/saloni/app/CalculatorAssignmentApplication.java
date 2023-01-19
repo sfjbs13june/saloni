396519b87183fc3450ac4ef7cfb4f3176ade46ec
sfjbs <salonigavade2000@gmail.com>
2023-01-19 17:36:26 +0000

    Day 05:

---
+++ Day05/calculator-assignment/calculator-assignment/src/main/java/com/saloni/app/CalculatorAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.saloni.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorAssignmentApplication.class, args);
+	}
+
+}
