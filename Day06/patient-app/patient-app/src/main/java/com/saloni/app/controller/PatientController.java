9e6c508853247484b2fe834bafedf0762ab11494
sfjbs <salonigavade2000@gmail.com>
2023-01-23 05:33:15 +0000

    Day 06:Patient-app

--- Day06/patient-app/patient-app/src/main/java/com/saloni/app/controller/PatientController.java
+++ Day06/patient-app/patient-app/src/main/java/com/saloni/app/controller/PatientController.java
@@ -3,15 +3,19 @@ package com.saloni.app.controller;
 import com.saloni.app.model.Patient;
 import com.saloni.app.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PatientController {
+    @Value("${app.name}")
+    String name;
 
     @Autowired
     PatientService patientService;
     @PostMapping ("/patient/save")
     public Patient savePatient(@RequestBody Patient patient){
+        System.out.println(name);
         System.out.println(patient);
         return patient;
     }
--- Day06/patient-app/patient-app/src/test/java/com/saloni/app/PatientControllerIntegrationTest.java
+++ Day06/patient-app/patient-app/src/test/java/com/saloni/app/PatientControllerIntegrationTest.java
@@ -1,4 +1,81 @@
 package com.saloni.app;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.saloni.app.controller.PatientController;
+import com.saloni.app.model.Patient;
+import com.saloni.app.service.PatientService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.util.Base64Utils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest({PatientController.class})
+@ActiveProfiles(value = "test")
 public class PatientControllerIntegrationTest {
+    @InjectMocks
+    PatientController patientController;
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    PatientService patientService;
+    @Value("${patient.post.url}")
+    String postUrl;
+
+    @Value("${patient.put.url}")
+    String putUrl;
+
+    @Test
+    public void testSaveStudentPost() throws Exception {
+        String secret = "Basic " + Base64Utils.encodeToString(("user123"+":"+"password").getBytes());
+        Patient patient=new Patient("pat01","pat_test","30","Male","fever");
+        ResultActions responseEntity  = mockMvc.perform(post(postUrl).header(HttpHeaders.AUTHORIZATION, secret).contentType(MediaType.APPLICATION_JSON)
+                .content(asObjectToJsonString(patient)).accept(MediaType.APPLICATION_JSON));
+        responseEntity.andExpect(status().isOk());
+        String result = ((ResultActions) responseEntity).andReturn().getResponse().getContentAsString();
+        Patient patientResult= asJsonStringToObject(result);
+        assertNotNull(patientResult);
+        assertEquals(patientResult.getId(),"pat01");
+        assertEquals(patientResult.getName(),"pat_test");
+        assertEquals(patientResult.getAge(),"30");
+        assertEquals(patientResult.getGender(),"Male");
+        assertEquals(patientResult.getDisease(),"fever");
+    }
+
+    private String asObjectToJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            final String jsonContent = mapper.writeValueAsString(obj);
+            return jsonContent;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Patient asJsonStringToObject( String result) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            Patient patient= mapper.readValue(result, Patient.class);
+            return patient;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
